aecfb7ce88892ada0e94c5c0f862952a30a95990
Ali NGame <ngame1390@hotmail.com>
2022-02-25 17:52:39 +0330

    added story ring color selector

---
+++ WinstaNext/Converters/Stories/StoryBorderColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using InstagramApiSharp.Classes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace WinstaNext.Converters.Stories
+{
+    internal class StoryBorderColorConverter : IValueConverter
+    {
+        public GradientBrush CloseFriends { get; set; }
+        public GradientBrush NormalStory { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value is InstaReelFeed story)
+            {
+                if (story.LatestReelMedia == story.Seen)
+                    return null;
+                if (story.HasBestiesMedia)
+                    return CloseFriends;
+                return NormalStory;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
