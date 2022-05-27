b50733d8c07ef9619a816eb981bb33216b95b8a3
Ali NGame <ngame1390@hotmail.com>
2022-05-28 02:06:39 +0430

    -Added highlight presenter user control

---
+++ WinstaNext/UI/Stories/InstaHighlightFeedPresenterUC.xaml.cs
@@ -0,0 +1,41 @@
+﻿using InstagramApiSharp.Classes.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using WinstaNext.Services;
+using WinstaNext.Views.Profiles;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WinstaNext.UI.Stories
+{
+    public sealed partial class InstaHighlightFeedPresenterUC : UserControl
+    {
+        public static readonly DependencyProperty HighlightFeedProperty = DependencyProperty.Register(
+             "HighlightFeed",
+             typeof(InstaHighlightFeed),
+             typeof(InstaHighlightFeedPresenterUC),
+             new PropertyMetadata(null));
+
+        public InstaHighlightFeed HighlightFeed
+        {
+            get { return (InstaHighlightFeed)GetValue(HighlightFeedProperty); }
+            set { SetValue(HighlightFeedProperty, value); }
+        }
+
+        public RelayCommand<object> NavigateToUserProfileCommand { get; set; }
+
+        public InstaHighlightFeedPresenterUC()
+        {
+            NavigateToUserProfileCommand = new(NavigateToUserProfile);
+            this.InitializeComponent();
+        }
+
+        void NavigateToUserProfile(object obj)
+        {
+            var NavigationService = App.Container.GetService<NavigationService>();
+            NavigationService.Navigate(typeof(UserProfileView), obj);
+        }
+    }
+}
