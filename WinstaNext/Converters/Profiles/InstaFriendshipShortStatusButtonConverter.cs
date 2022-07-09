e2c7f8a74d8885c697d759ce7b59de7cdfdefcf4
Ali NGame <ngame1390@hotmail.com>
2022-07-09 23:11:21 +0430

    -Direct follow from search

---
+++ WinstaNext/Converters/Profiles/InstaFriendshipShortStatusButtonConverter.cs
@@ -0,0 +1,38 @@
+﻿using InstagramApiSharp.Classes.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Windows.UI.Xaml.Data;
+using WinstaNext.Services;
+using WinstaNext.ViewModels.Users;
+using WinstaNext.Views.Profiles;
+
+namespace WinstaNext.Converters.Profiles
+{
+    internal class InstaFriendshipShortStatusButtonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not InstaFriendshipShortStatus FriendshipStatus || FriendshipStatus == null) return string.Empty;
+            if (App.Container.GetService<NavigationService>().Content is UserProfileView userProfile)
+            {
+                if (userProfile.DataContext is UserProfileViewModel viewModel)
+                    if (viewModel.User.Pk == App.Container.GetService<InstaUserShort>().Pk)
+                        return LanguageManager.Instance.Instagram.EditProfile;
+            }
+            //if (User.Pk == App.Container.GetService<InstaUserShort>().Pk)
+            //    return LanguageManager.Instance.Instagram.EditProfile;
+            if (FriendshipStatus.OutgoingRequest)
+                return LanguageManager.Instance.Instagram.Requested;
+            else if (!FriendshipStatus.Following)
+                return LanguageManager.Instance.Instagram.Follow;
+            else if (FriendshipStatus.Following)
+                return LanguageManager.Instance.Instagram.Unfollow;
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WinstaNext/UI/Search/PeopleSearchUC.xaml.cs
+++ WinstaNext/UI/Search/PeopleSearchUC.xaml.cs
@@ -1,9 +1,16 @@
-﻿using InstagramApiSharp.Classes.Models;
+﻿using InstagramApiSharp.API;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp.Enums;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -21,7 +28,7 @@ namespace WinstaNext.UI.Search
     public sealed partial class PeopleSearchUC : UserControl
     {
         public static readonly DependencyProperty UserProperty = DependencyProperty.Register(
-          "User",
+          nameof(User),
           typeof(InstaUser),
           typeof(PeopleSearchUC),
           new PropertyMetadata(null));
@@ -32,9 +39,41 @@ namespace WinstaNext.UI.Search
             set { SetValue(UserProperty, value); }
         }
 
+        public AsyncRelayCommand FollowButtonCommand { get; set; }
+
+
         public PeopleSearchUC()
         {
             this.InitializeComponent();
+            FollowButtonCommand = new(FollowAsync);
+        }
+
+        public async Task FollowAsync()
+        {
+            if (FollowButtonCommand.IsRunning) return;
+            if (User.Pk == App.Container.GetService<InstaUserShort>().Pk)
+            {
+                //Edit profile
+                throw new NotSupportedException();
+            }
+            if (User.FriendshipStatus == null)
+                throw new ArgumentNullException(nameof(User.FriendshipStatus));
+
+            var follow = !User.FriendshipStatus.Following;
+
+            IResult<InstaFriendshipFullStatus> result;
+            using (IInstaApi Api = App.Container?.GetService<IInstaApi>())
+            {
+                if (follow)
+                    result = await Api.UserProcessor.FollowUserAsync(User.Pk,
+                             surfaceType: InstaMediaSurfaceType.Profile,
+                             mediaIdAttribution: null);
+                else result = await Api.UserProcessor.UnFollowUserAsync(User.Pk,
+                             surfaceType: InstaMediaSurfaceType.Profile,
+                             mediaIdAttribution: null);
+            }
+            if (!result.Succeeded) throw result.Info.Exception;
+            User.FriendshipStatus = result.Value.Adapt<InstaFriendshipShortStatus>();
         }
     }
 }
