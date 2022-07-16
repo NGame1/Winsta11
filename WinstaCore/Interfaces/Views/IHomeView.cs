65b3e6abfb8444c9ab7027596a350dbd5617ef05
Ali NGame <ngame1390@hotmail.com>
2022-07-17 02:41:02 +0430

    -Added IView to inherrit views from it. (used for cache manager)

--- WinstaCore/Interfaces/Views/IHomeView.cs
+++ WinstaCore/Interfaces/Views/IHomeView.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WinstaCore.Attributes;
+﻿using WinstaCore.Attributes;
 
 namespace WinstaCore.Interfaces.Views
 {
-    public interface IHomeView
+    public interface IHomeView : IView
     {
         RangePlayerAttribute Medias { get; }
     }
