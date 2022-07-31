e049d2bf2d892c11a14593898f85a458f2eee502
Ali NGame <ngame1390@hotmail.com>
2022-08-01 02:59:27 +0430

    -Fixed System.Threading.Thread load error on WinstaMobile.

--- WinstaMobile/ViewModels/Media/InstaMediaPresenterUCViewModel.cs
+++ WinstaMobile/ViewModels/Media/InstaMediaPresenterUCViewModel.cs
@@ -128,13 +128,13 @@ namespace WinstaMobile.ViewModels.Media
                 {
                     LoadLikeAnimation = true;
                     Media.LikesCount++;
-                    new Thread(UnloadLikeAnimation).Start();
+                    Task.Factory.StartNew(UnloadLikeAnimation);
                 }
                 else
                 {
                     LoadUnLikeAnimation = true;
                     Media.LikesCount--;
-                    new Thread(UnloadLikeAnimation).Start();
+                    Task.Factory.StartNew(UnloadLikeAnimation);
                 }
                 Media.HasLiked = !Media.HasLiked;
                 var Frame = NavigationService.Content;
