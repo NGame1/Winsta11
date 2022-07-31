58cd6bd234d4578799d7bd607aef495b89b19d4d
Ali NGame <ngame1390@hotmail.com>
2022-08-01 02:58:19 +0430

    -Added save and like button user controls to WinstaMobile

---
+++ WinstaMobile/UI/Generic/LikeButtonControl.xaml.cs
@@ -0,0 +1,84 @@
+﻿using PropertyChanged;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WinstaMobile.UI.Generic
+{
+    [AddINotifyPropertyChangedInterface]
+    public sealed partial class LikeButtonControl : Button
+    {
+        public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register(
+                nameof(Glyph),
+                typeof(string),
+                typeof(LikeButtonControl),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty GlyphFontProperty = DependencyProperty.Register(
+                nameof(GlyphFont),
+                typeof(FontFamily),
+                typeof(LikeButtonControl),
+                new PropertyMetadata(null));
+
+        //public static readonly DependencyProperty GlyphForegroundProperty = DependencyProperty.Register(
+        //           nameof(GlyphForeground),
+        //           typeof(SolidColorBrush),
+        //           typeof(LikeButtonControl),
+        //           new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IsLikedProperty = DependencyProperty.Register(
+             "IsLiked",
+             typeof(bool),
+             typeof(LikeButtonControl),
+             new PropertyMetadata(null));
+
+        public string Glyph
+        {
+            get { return (string)GetValue(GlyphProperty); }
+            set { SetValue(GlyphProperty, value); }
+        }
+
+        public FontFamily GlyphFont
+        {
+            get { return (FontFamily)GetValue(GlyphFontProperty); }
+            set { SetValue(GlyphFontProperty, value); }
+        }
+
+        //public SolidColorBrush GlyphForeground
+        //{
+        //    get { return (SolidColorBrush)GetValue(GlyphForegroundProperty); }
+        //    set { SetValue(GlyphForegroundProperty, value); }
+        //}
+
+        [OnChangedMethod(nameof(OnLikedChanged))]
+        public bool IsLiked
+        {
+            get { return (bool)GetValue(IsLikedProperty); }
+            set { SetValue(IsLikedProperty, value); }
+        }
+
+        public LikeButtonControl()
+        {
+            this.InitializeComponent();
+            OnLikedChanged();
+        }
+
+        void OnLikedChanged()
+        {
+            if (!IsLiked)
+            {
+                GlyphFont = (FontFamily)App.Current.Resources["FluentSystemIconsRegular"];
+                //GlyphForeground = (SolidColorBrush)App.Current.Resources.ThemeDictionaries["ApplicationForegroundThemeBrush"];
+                Glyph = "\u0363";
+            }
+            else
+            {
+                GlyphFont = (FontFamily)App.Current.Resources["FluentSystemIconsFilled"];
+                //GlyphForeground = new SolidColorBrush(Colors.Red);
+                Glyph = "\u036F";
+            }
+        }
+    }
+}
---
+++ WinstaMobile/UI/Generic/SaveButtonControl.xaml.cs
@@ -0,0 +1,85 @@
+﻿using PropertyChanged;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WinstaMobile.UI.Generic
+{
+    [AddINotifyPropertyChangedInterface]
+    public sealed partial class SaveButtonControl : Button
+    {
+        public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register(
+                   nameof(Glyph),
+                   typeof(string),
+                   typeof(LikeButtonControl),
+                   new PropertyMetadata(null));
+
+        public static readonly DependencyProperty GlyphFontProperty = DependencyProperty.Register(
+                nameof(GlyphFont),
+                typeof(FontFamily),
+                typeof(LikeButtonControl),
+                new PropertyMetadata(null));
+
+        //public static readonly DependencyProperty GlyphForegroundProperty = DependencyProperty.Register(
+        //           nameof(GlyphForeground),
+        //           typeof(SolidColorBrush),
+        //           typeof(LikeButtonControl),
+        //           new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IsSavedProperty = DependencyProperty.Register(
+             nameof(IsSaved),
+             typeof(bool),
+             typeof(LikeButtonControl),
+             new PropertyMetadata(null));
+
+        public string Glyph
+        {
+            get { return (string)GetValue(GlyphProperty); }
+            set { SetValue(GlyphProperty, value); }
+        }
+
+        public FontFamily GlyphFont
+        {
+            get { return (FontFamily)GetValue(GlyphFontProperty); }
+            set { SetValue(GlyphFontProperty, value); }
+        }
+
+        //public SolidColorBrush GlyphForeground
+        //{
+        //    get { return (SolidColorBrush)GetValue(GlyphForegroundProperty); }
+        //    set { SetValue(GlyphForegroundProperty, value); }
+        //}
+
+        [OnChangedMethod(nameof(OnSavedChanged))]
+        public bool IsSaved
+        {
+            get { return (bool)GetValue(IsSavedProperty); }
+            set { SetValue(IsSavedProperty, value); }
+        }
+
+        public SaveButtonControl()
+        {
+            this.InitializeComponent();
+            OnSavedChanged();
+        }
+
+        void OnSavedChanged()
+        {
+            if (!IsSaved)
+            {
+                GlyphFont = (FontFamily)App.Current.Resources["FluentSystemIconsRegular"];
+                //GlyphForeground = (SolidColorBrush)App.Current.Resources.ThemeDictionaries["ApplicationForegroundThemeBrush"];
+                Glyph = "\uF1F6";
+            }
+            else
+            {
+                GlyphFont = (FontFamily)App.Current.Resources["FluentSystemIconsFilled"];
+                //GlyphForeground = new SolidColorBrush(Colors.Red);
+                Glyph = "\uF1F6";
+            }
+        }
+
+    }
+}
