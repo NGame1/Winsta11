9a49835b3bd13f7068004492e96956d92e0a185b
Ali NGame <ngame1390@hotmail.com>
2022-08-02 01:46:05 +0430

    Fixed search crash on WinstaMobile

---
+++ WinstaMobile/UI/Generic/InstaUserPresenterUC.xaml.cs
@@ -0,0 +1,48 @@
+﻿using InstagramApiSharp.Classes.Models;
+using PropertyChanged;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WinstaMobile.UI.Generic
+{
+    [AddINotifyPropertyChangedInterface]
+    public sealed partial class InstaUserPresenterUC : UserControl
+    {
+        public static readonly DependencyProperty UserShortProperty = DependencyProperty.Register(
+          nameof(UserShort),
+          typeof(InstaUserShort),
+          typeof(InstaUserPresenterUC),
+          new PropertyMetadata(null));
+
+        [OnChangedMethod(nameof(OnUserShortChanged))]
+        public InstaUserShort UserShort
+        {
+            get { return (InstaUserShort)GetValue(UserShortProperty); }
+            set { SetValue(UserShortProperty, value); }
+        }
+
+        public static readonly DependencyProperty UserProperty = DependencyProperty.Register(
+          nameof(User),
+          typeof(InstaUser),
+          typeof(InstaUserPresenterUC),
+          new PropertyMetadata(null));
+
+        public InstaUser User
+        {
+            get { return (InstaUser)GetValue(UserProperty); }
+            set { SetValue(UserProperty, value); }
+        }
+
+        void OnUserShortChanged()
+        {
+            User = new(UserShort);
+        }
+
+        public InstaUserPresenterUC()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
