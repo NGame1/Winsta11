e3c7acca8dbac99c4f91f24b05e7b08462dcf5ec
Ali NGame <ngame1390@hotmail.com>
2022-08-06 23:41:23 +0430

    ProfilePictureConverter added

---
+++ WinstaNext.Converters/Media/InstaUserProfilePictureConverter.cs
@@ -0,0 +1,30 @@
+﻿using InstagramApiSharp.Classes.Models;
+using System;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace WinstaNext.Converters.Media
+{
+    public class InstaUserProfilePictureConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new BitmapImage(new Uri(value switch
+            {
+                InstaUser instaUser => instaUser.ProfilePictureId != "unknown" ?
+                          instaUser.ProfilePicUrl : "ms-appx:///Assets/Icons/NoOne.png",
+
+                InstaUserShort instaUserShort => instaUserShort.ProfilePictureId != "unknown" ?
+                               instaUserShort.ProfilePicUrl : "ms-appx:///Assets/Icons/NoOne.png",
+
+                _ => "ms-appx:///Assets/Icons/NoOne.png"
+
+            }, UriKind.RelativeOrAbsolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
