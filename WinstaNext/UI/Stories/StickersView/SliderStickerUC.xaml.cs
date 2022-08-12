914d5869e9c393461064613fcf7d319d232bfdd1
Ali NGame <ngame1390@hotmail.com>
2022-08-12 13:32:05 +0430

    -Fixed Slider emoji on story

--- WinstaNext/UI/Stories/StickersView/SliderStickerUC.xaml.cs
+++ WinstaNext/UI/Stories/StickersView/SliderStickerUC.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using InstagramApiSharp.Classes.Models;
 using Microsoft.Toolkit.Uwp.UI;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
@@ -52,19 +54,49 @@ namespace WinstaNext.UI.Stories.StickersView
             this.FontSize = Math.Round(24 / (Slider.Height / scale));
 
             Thickness marg = new(
-                0, 
+                0,
                 Math.Round(14 * (Slider.Height / scale)),
-                0, 
+                0,
                 Math.Round(14 * (Slider.Height / scale)));
 
             Thickness sliderMarg = new(
                 Math.Round(14 * (Slider.Width / scaleY)),
-                Math.Round(0 * (Slider.Height / scale)), 
+                Math.Round(0 * (Slider.Height / scale)),
                 Math.Round(14 * (Slider.Width / scaleY)),
                 Math.Round(0 * (Slider.Height / scale)));
             slider.Margin = sliderMarg;
             //this.Margin = marg;
 
+        }
+
+        private async void slider_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(10);
+            var T4 = slider.FindDescendantOrSelf<TextBlock>();
+            if (T4 != null)
+                T4.Text = Slider.SliderSticker.Emoji;
+        }
+
+        static T FindChildOfType<T>(DependencyObject root) where T : class
+        {
+            var queue = new Queue<DependencyObject>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                if (current == null) return null;
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(current); i++)
+                {
+                    var child = VisualTreeHelper.GetChild(current, i);
+                    var typedChild = child as T;
+                    if (typedChild != null)
+                        return typedChild;
+
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
         }
     }
 }
