75a1a92ab734a87903d1cdc2653265c5927c4b98
Ali NGame <ngame1390@hotmail.com>
2022-08-19 01:49:00 +0430

    -Added question reply in story by pressing Enter.

--- WinstaNext/UI/Stories/StickersView/QuestionStickerUC.xaml.cs
+++ WinstaNext/UI/Stories/StickersView/QuestionStickerUC.xaml.cs
@@ -1,7 +1,14 @@
-﻿using InstagramApiSharp.Classes.Models;
+﻿using InstagramApiSharp;
+using InstagramApiSharp.API;
+using InstagramApiSharp.Classes.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Toolkit.Uwp.UI;
+using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using WinstaCore;
+using static System.Net.Mime.MediaTypeNames;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -39,6 +46,30 @@ namespace WinstaNext.UI.Stories.StickersView
         {
             var parent = this.FindParent<StickersViewGrid>();
             parent?.ResumeTimerCommand.Execute(null);
+        }
+        async void AnswerQuestionAsync(string StoryId, long QuestionId, string reply)
+        {
+            using (IInstaApi Api = AppCore.Container.GetService<IInstaApi>())
+            {
+                var result = await Api.StoryProcessor.AnswerToStoryQuestionAsync(StoryId, QuestionId, reply);
+                if (!result.Succeeded)
+                {
+                    throw result.Info.Exception;
+                }
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key != VirtualKey.Enter) return;
+            var state = Window.Current.CoreWindow.GetKeyState(VirtualKey.Shift);
+            if (!state.HasFlag(CoreVirtualKeyStates.Down))
+            {
+                if (sender is not TextBox txt) return;
+                e.Handled = true;
+                var parent = this.FindParent<InstaStoryItemPresenterUC>();
+                AnswerQuestionAsync(parent.Story.Id, Question.QuestionSticker.QuestionId, txt.Text);
+            }
         }
     }
 }
