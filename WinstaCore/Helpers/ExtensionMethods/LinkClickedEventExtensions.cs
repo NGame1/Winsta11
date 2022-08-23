f13d698d4d3b4720417224dd08a85215312cb63e
Ali NGame <ngame1390@hotmail.com>
2022-08-24 00:02:52 +0430

    -Better URL handling in captions [Both with http(s) prefixes and without them] using Regex

--- WinstaCore/Helpers/ExtensionMethods/LinkClickedEventExtensions.cs
+++ WinstaCore/Helpers/ExtensionMethods/LinkClickedEventExtensions.cs
@@ -3,6 +3,10 @@ using Windows.System;
 using WinstaCore.Interfaces.Views.Profiles;
 using WinstaCore.Services;
 using Microsoft.Toolkit.Uwp.UI.Controls;
+using System.Text.RegularExpressions;
+using WinstaCore.Constants;
+using InstagramApiSharp.Classes;
+using System.Threading;
 
 namespace WinstaCore.Helpers.ExtensionMethods
 {
@@ -11,6 +15,7 @@ namespace WinstaCore.Helpers.ExtensionMethods
         public static async void HandleClickEvent(this LinkClickedEventArgs obj, NavigationService navigation = null)
         {
             NavigationService NavigationService;
+            var matches = Regex.Matches(obj.Link.ToLower(), RegexConstants.WebUrlRegex);
             if (navigation == null)
                 NavigationService = AppCore.Container.GetService<NavigationService>();
             else NavigationService = navigation;
@@ -26,10 +31,18 @@ namespace WinstaCore.Helpers.ExtensionMethods
                 NavigationService.Navigate(HashtagProfileView,
                                   obj.Link.Replace("#", string.Empty));
             }
-            else if (obj.Link.StartsWith("http://") || obj.Link.StartsWith("https://"))
+            else if (Regex.Match(obj.Link, RegexConstants.WebUrlRegex) is Match match && match.Groups.Count > 1)
             {
-                await Launcher.LaunchUriAsync(new Uri(obj.Link, UriKind.RelativeOrAbsolute));
+                await Launcher.LaunchUriAsync(new Uri(match.Value, UriKind.RelativeOrAbsolute));
             }
+            else if (Regex.Match(obj.Link, RegexConstants.WebUrlWithoutPrefixRegex) is Match match2 && match2.Groups.Count > 1)
+            {
+                await Launcher.LaunchUriAsync(new Uri(match2.Value, UriKind.RelativeOrAbsolute));
+            }
+            //else if (obj.Link.StartsWith("http://") || obj.Link.StartsWith("https://"))
+            //{
+            //    await Launcher.LaunchUriAsync(new Uri(obj.Link, UriKind.RelativeOrAbsolute));
+            //}
         }
     }
 }
