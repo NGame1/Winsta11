0cdd06d6bc0ad9f77cb5cf54aea2b531597926a0
Ali NGame <ngame1390@hotmail.com>
2022-08-24 00:10:18 +0430

    -Added support for tapping on Direct images to view them in full-screen mode. #12

--- WinstaNext/UI/Directs/MessageContainer/MediaMessageContainerUC.xaml.cs
+++ WinstaNext/UI/Directs/MessageContainer/MediaMessageContainerUC.xaml.cs
@@ -2,6 +2,10 @@
 using System;
 using Windows.Media.Core;
 using Windows.UI.Xaml;
+using WinstaCore.Interfaces.Views.Medias;
+using WinstaCore;
+using Microsoft.Extensions.DependencyInjection;
+using WinstaCore.Services;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -28,6 +32,13 @@ namespace WinstaNext.UI.Directs.MessageContainer
                     MediaSource.CreateFromUri(
                         new Uri(DirectItem.Media.Videos[0].Uri, UriKind.RelativeOrAbsolute)));
             }
+        }
+
+        private void imgMedia_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var NavigationService = AppCore.Container.GetService<NavigationService>();
+            var ImageViewerPage = AppCore.Container.GetService<IImageViewerPage>();
+            NavigationService.Navigate(ImageViewerPage, DirectItem.Media.Images[0].Uri);
         }
     }
 }
