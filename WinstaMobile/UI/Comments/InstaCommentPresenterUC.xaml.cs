1d9dee3e576170fadb748bb4f68cb2c47d52b8ac
Ali NGame <ngame1390@hotmail.com>
2022-08-25 00:29:15 +0430

    -Added comments to Wingo Tag Mobile

---
+++ WinstaMobile/UI/Comments/InstaCommentPresenterUC.xaml.cs
@@ -0,0 +1,155 @@
+﻿using InstagramApiSharp.Classes.Models;
+using InstagramApiSharp;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using InstagramApiSharp.API;
+using InstagramApiSharp.Classes;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using WinstaCore.Services;
+using WinstaCore.Helpers.ExtensionMethods;
+using WinstaMobile.Views.Comments;
+using WinstaMobile.Views.Profiles;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace WinstaMobile.UI.Comments
+{
+    public sealed partial class InstaCommentPresenterUC : UserControl
+    {
+        public static readonly DependencyProperty CommentProperty = DependencyProperty.Register(
+          nameof(Comment),
+          typeof(InstaComment),
+          typeof(InstaCommentPresenterUC),
+          new PropertyMetadata(null));
+
+        public InstaComment Comment
+        {
+            get { return (InstaComment)GetValue(CommentProperty); }
+            set { SetValue(CommentProperty, value); }
+        }
+
+        PaginationParameters Pagination { get; set; } = PaginationParameters.MaxPagesToLoad(1);
+
+        public AsyncRelayCommand LikeCommecntCommand { get; set; }
+        public AsyncRelayCommand LoadMoreCommentsCommand { get; set; }
+        public RelayCommand ReplyCommand { get; set; }
+
+        public RelayCommand<object> NavigateToUserProfileCommand { get; set; }
+        public RelayCommand NavigateToCommentLikersCommand { get; set; }
+
+        public RelayCommand<LinkClickedEventArgs> CaptionLinkClickedCommand { get; set; }
+
+        public InstaCommentPresenterUC()
+        {
+            CaptionLinkClickedCommand = new(CaptionLinkClicked);
+            LoadMoreCommentsCommand = new(LoadMoreCommentsAsync);
+            LikeCommecntCommand = new(LikeCommentAsync);
+            ReplyCommand = new(Reply);
+            NavigateToUserProfileCommand = new(NavigateToUserProfile);
+            NavigateToCommentLikersCommand = new(NavigateToCommentLikers);
+            this.InitializeComponent();
+        }
+        void CaptionLinkClicked(LinkClickedEventArgs obj)
+            => obj.HandleClickEvent();
+
+        async Task LikeCommentAsync()
+        {
+            IResult<bool> result = null;
+            bool liked = Comment.HasLikedComment;
+            var likesCount = Comment.LikesCount;
+            Comment.HasLikedComment = !Comment.HasLikedComment;
+            try
+            {
+                using (IInstaApi Api = App.Container.GetService<IInstaApi>())
+                {
+                    if (liked)
+                    {
+                        Comment.LikesCount--;
+                        result = await Api.CommentProcessor.UnlikeCommentAsync(Comment.Pk.ToString());
+                    }
+                    else
+                    {
+                        Comment.LikesCount++;
+                        result = await Api.CommentProcessor.LikeCommentAsync(Comment.Pk.ToString());
+                    }
+                }
+            }
+            finally
+            {
+                if (!result.Succeeded)
+                {
+                    Comment.HasLikedComment = liked;
+                    Comment.LikesCount = likesCount;
+                }
+            }
+        }
+
+        async Task LoadMoreCommentsAsync()
+        {
+            if (LoadMoreCommentsCommand.IsRunning) return;
+            var NavigationService = App.Container.GetService<NavigationService>();
+            string mediaId = string.Empty;
+            if (NavigationService.Content is MediaCommentsView view)
+            {
+                mediaId = view.MediaId;
+            }
+            try
+            {
+                using (IInstaApi Api = App.Container.GetService<IInstaApi>())
+                {
+                    var result = await Api.CommentProcessor.GetMediaRepliesCommentsAsync(mediaId,
+                                                            Comment.Pk.ToString(), Pagination);
+
+                    if (!result.Succeeded) throw result.Info.Exception;
+
+                    var childs = result.Value.ChildComments;
+                    //childs.Reverse();
+                    for (int i = 0; i < childs.Count; i++)
+                    {
+                        if (!Comment.ChildComments.Any(x => x.Pk == childs.ElementAt(i).Pk))
+                            Comment.ChildComments.Add(childs.ElementAt(i));
+                    }
+                    Comment.HasMoreTailChildComments = result.Value.HasMoreTailChildComments;
+                    Comment.HasMoreHeadChildComments = result.Value.HasMoreHeadChildComments;
+                }
+            }
+            finally
+            {
+            }
+        }
+
+        void Reply()
+        {
+            try
+            {
+                var NavigationService = App.Container.GetService<NavigationService>();
+                string mediaId = string.Empty;
+                if (NavigationService.Content is MediaCommentsView view)
+                {
+                    mediaId = view.MediaId;
+                    view.ViewModel.ReplyedComment = this.Comment;
+                }
+
+            }
+            finally
+            {
+            }
+        }
+
+        void NavigateToUserProfile(object obj)
+        {
+            var NavigationService = App.Container.GetService<NavigationService>();
+            NavigationService.Navigate(typeof(UserProfileView), obj);
+        }
+
+        void NavigateToCommentLikers()
+        {
+            var NavigationService = App.Container.GetService<NavigationService>();
+            NavigationService.Navigate(typeof(CommentLikersView), this.Comment.Pk.ToString());
+        }
+    }
+}
--- WinstaMobile/Views/Comments/CommentLikersView.xaml.cs
+++ WinstaMobile/Views/Comments/CommentLikersView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Resources;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,13 +20,9 @@ namespace WinstaMobile.Views.Comments
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MediaCommentsView : BasePage
+    public sealed partial class CommentLikersView : Page
     {
-        public override string PageHeader { get; protected set; } = LanguageManager.Instance.Instagram.MediaComments;
-
-        public string MediaId { get => ViewModel.Media.InstaIdentifier; }
-
-        public MediaCommentsView()
+        public CommentLikersView()
         {
             this.InitializeComponent();
         }
