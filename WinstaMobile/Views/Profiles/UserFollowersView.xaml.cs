91ce2fe9a9344b84fa3de09d22952640d93cc73c
Ali NGame <ngame1390@hotmail.com>
2022-08-26 01:23:04 +0430

    -Added user followers view

---
+++ WinstaMobile/Views/Profiles/UserFollowersView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Resources;
+using WinstaCore.Interfaces.Views.Profiles;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WinstaMobile.Views.Profiles
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UserFollowersView : BasePage, IUserFollowersView
+    {
+        public override string PageHeader { get; protected set; } = LanguageManager.Instance.Instagram.Followers;
+
+        public UserFollowersView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
