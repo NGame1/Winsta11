b1132cdb7833a39e7eff6311fb7a70750b162116
Ali NGame <ngame1390@hotmail.com>
2022-11-27 00:38:32 +0330

    Some Extension methods for FFMPEG

---
+++ WinstaCore/Helpers/FFMpegHelpers/FFMpegAudioHelpers.cs
@@ -0,0 +1,34 @@
+﻿using FFmpegInteropX;
+using Microsoft.Toolkit.Uwp.UI;
+using System;
+using Windows.System;
+
+namespace WinstaCore.Helpers.FFMpegHelpers
+{
+    public static class FFMpegAudioHelpers
+    {
+        /// <summary>
+        /// Apply echoing to the input audio.
+        /// Echoes are reflected sound and can occur naturally amongst mountains(and sometimes large buildings) when talking or shouting; digital echo effects emulate this behaviour and are often used to help fill out the sound of a single instrument or vocal.The time difference between the original signal and the reflection is the delay, and the loudness of the reflected signal is the decay. Multiple echoes can have different delays and decays.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="inGain">Set input gain of reflected signal. Default is 0.6.</param>
+        /// <param name="outGain">Set output gain of reflected signal. Default is 0.3.</param>
+        /// <param name="delayMs">Set list of time intervals in milliseconds between original signal and reflections separated by ’|’. Allowed range for each delay is (0 - 90000.0]. Default is 1000.</param>
+        /// <param name="decays">Set list of loudness of reflected signals separated by ’|’. Allowed range for each decay is (0 - 1.0]. Default is 0.5.</param>
+        public static void Echo(this FFmpegMediaSource source, float inGain = 0.6f, float outGain = 0.3f, int delayMs = 1000, float decays = 0.5f)
+        {
+            source.SetFFmpegAudioFilters($"aecho=0.8:0.88:{delayMs}:0.4");
+        }
+
+        /// <summary>
+        /// Simple audio dynamic range compression/expansion filter.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="contrast">Set contrast. Default is 33. Allowed range is between 0 and 100.</param>
+        public static void Contrast(this FFmpegMediaSource source, int contrast = 33)
+        {
+            source.SetFFmpegAudioFilters($"acontrast=contrast={contrast}");
+        }
+    }
+}
---
+++ WinstaCore/Helpers/FFMpegHelpers/FFMpegVideoHelpers.cs
@@ -0,0 +1,32 @@
+﻿using FFmpegInteropX;
+
+namespace WinstaCore.Helpers.FFMpegHelpers
+{
+    public static class FFMpegVideoHelpers
+    {
+        /// <summary>
+        /// Crop the input video to given dimensions.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="width">The width of the output video. It defaults to iw. This expression is evaluated only once during the filter configuration, or when the ‘w’ or ‘out_w’ command is sent.</param>
+        /// <param name="height">The height of the output video. It defaults to ih. This expression is evaluated only once during the filter configuration, or when the ‘h’ or ‘out_h’ command is sent.</param>
+        /// <param name="x">The horizontal position, in the input video, of the left edge of the output video. It defaults to (in_w-out_w)/2. This expression is evaluated per-frame.</param>
+        /// <param name="y">The vertical position, in the input video, of the top edge of the output video. It defaults to (in_h-out_h)/2. This expression is evaluated per-frame.</param>
+        public static void CropVideo(this FFmpegMediaSource source, int width, int height, int x, int y)
+        {
+            source.SetFFmpegVideoFilters($"crop=w={width}:h={height}:x={x}:y={y}");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="sizeX"></param>
+        /// <param name="sizeY"></param>
+        /// <param name="planes"></param>
+        public static void Blur(this FFmpegMediaSource source, int sizeX = 0, int sizeY = 0, int planes = 15)
+        {
+            source.SetFFmpegVideoFilters($"avgblur=sizeX={sizeX}:sizeY={sizeY}:planes={planes}");
+        }
+    }
+}
