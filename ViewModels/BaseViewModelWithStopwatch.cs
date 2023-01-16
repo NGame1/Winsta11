6031d865f531f3e1b3830a4cde7e0bc7376c7ac4
Ali NGame <ngame1390@hotmail.com>
2023-01-16 22:26:12 +0330

    - Added a new BaseViewModel to work with stopwatch

---
+++ ViewModels/BaseViewModelWithStopwatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Navigation;
+
+namespace ViewModels;
+
+public class BaseViewModelWithStopwatch : BaseViewModel
+{
+    protected Stopwatch StopTimer { get; set; }
+
+    public override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        StopTimer = Stopwatch.StartNew();
+        base.OnNavigatedTo(e);
+    }
+
+    public override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+    {
+        StopTimer.Stop();
+        StopTimer = null;
+        base.OnNavigatingFrom(e);
+    }
+
+    public async Task<bool> EnsureTimeElapsed(int Milliseconds = 500)
+    {
+        StopTimer.Restart();
+        await Task.Delay(Milliseconds);
+        if (StopTimer.ElapsedMilliseconds < Milliseconds) return false;
+        return true;
+    }
+}
