9f99db718a14652d91fbee19a181a5fa78da7698
Ali NGame <ngame1390@hotmail.com>
2023-01-29 19:29:52 +0330

    - Added int support for Possible Long Number Converter

--- WinstaNext.Converters/PossibleLargeNumbersConverter.cs
+++ WinstaNext.Converters/PossibleLargeNumbersConverter.cs
@@ -2,24 +2,26 @@
 using System;
 using Windows.UI.Xaml.Data;
 
-namespace WinstaNext.Converters
-{
-    public class PossibleLargeNumbersConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            if (value is not long val) return string.Empty;
-            if (val < 1_000) return val.ToString();
-            if (val < 1_000_000) return $"{(val / 1_000f).ToString("#,##0.0")} {LanguageManager.Instance.Units.Kilo}";
-            if (val < 1_000_000_000) return $"{(val / 1_000_000f).ToString("#,##0.0")} {LanguageManager.Instance.Units.Million}";
-            if (val < 1_000_000_000_000) return $"{(val / 1_000_000_000f).ToString("#,##0.0")} {LanguageManager.Instance.Units.Billion}";
-            //1000000.ToString("N", CultureInfo.CreateSpecificCulture(language));
-            return string.Empty;
-        }
+namespace WinstaNext.Converters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            throw new NotImplementedException();
-        }
+public class PossibleLargeNumbersConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        long val;
+        if (value is long @long) val = @long;
+        else if (value is int @int) val = @int;
+        else return string.Empty;
+        if (val < 1_000) return val.ToString();
+        if (val < 1_000_000) return $"{(val / 1_000f).ToString("#,##0.0")} {LanguageManager.Instance.Units.Kilo}";
+        if (val < 1_000_000_000) return $"{(val / 1_000_000f).ToString("#,##0.0")} {LanguageManager.Instance.Units.Million}";
+        if (val < 1_000_000_000_000) return $"{(val / 1_000_000_000f).ToString("#,##0.0")} {LanguageManager.Instance.Units.Billion}";
+        //1000000.ToString("N", CultureInfo.CreateSpecificCulture(language));
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
     }
 }
