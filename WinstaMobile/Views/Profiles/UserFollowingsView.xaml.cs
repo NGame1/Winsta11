313c04f2034bf7c1978268c31a04b9d50e27e896
Ali NGame <ngame1390@hotmail.com>
2023-07-20 10:40:53 +0330

    - Fixed a bug in removing follower and unfollowing from user profile view.

--- WinstaMobile/Views/Profiles/UserFollowingsView.xaml.cs
+++ WinstaMobile/Views/Profiles/UserFollowingsView.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Resources;
+﻿using InstagramApiSharp.API;
+using InstagramApiSharp.Classes.Models;
+using Resources;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+using WinstaCore;
 using WinstaCore.Interfaces.Views.Profiles;
+using Microsoft.Extensions.DependencyInjection;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,6 +21,16 @@ namespace WinstaMobile.Views.Profiles
         public UserFollowingsView()
         {
             this.InitializeComponent();
+        }
+
+        private async void InstaUserPresenterUC_RemoveButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button btnRemove) return;
+            if (btnRemove.DataContext is not InstaUserShort userShort) return;
+            using IInstaApi Api = AppCore.Container.GetService<IInstaApi>();
+            var result = await Api.UserProcessor.UnFollowUserAsync(userShort.Pk);
+            if (!result.Succeeded) return;
+            ViewModel.UserFollowings.Remove(userShort);
         }
     }
 }
