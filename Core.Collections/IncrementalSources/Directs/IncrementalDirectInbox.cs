cd769cbc3ebbfb9affd9843426731517b073cebb
Ali NGame <ngame1390@hotmail.com>
2023-09-03 21:03:18 +0330

    - Fixed a bug in directs

--- Core.Collections/IncrementalSources/Directs/IncrementalDirectInbox.cs
+++ Core.Collections/IncrementalSources/Directs/IncrementalDirectInbox.cs
@@ -23,20 +23,34 @@ namespace Core.Collections.IncrementalSources.Directs
         }
 
         bool nomoreitems = false;
+        bool _isLoading = false;
         public async Task<IEnumerable<InstaDirectInboxThread>> GetPagedItemsAsync(int pageIndex, int pageSize, CancellationToken cancellationToken = default)
         {
             if (nomoreitems) return null;
-            using (IInstaApi Api = AppCore.Container.GetService<IInstaApi>())
+            using IInstaApi Api = AppCore.Container.GetService<IInstaApi>();
+            while (_isLoading)
             {
+                await Task.Delay(200);
+            }
+            try
+            {
+                var currentNextMaxId = Pagination.NextMaxId;
+                _isLoading = true;
                 var result = await Api.MessagingProcessor.GetDirectInboxAsync(Pagination,
-                    cancellationToken: cancellationToken);
+                cancellationToken: cancellationToken);
                 if (!result.Succeeded && result.Info.Exception is not TaskCanceledException)
                     throw result.Info.Exception;
                 if (!result.Value.Inbox.HasOlder) nomoreitems = true;
                 if (result.Value.SeqId > 0)
                     SeqId = result.Value.SeqId;
                 SnapshotAt = result.Value.SnapshotAt;
+                if (currentNextMaxId == Pagination.NextMaxId)
+                    return null;
                 return result.Value.Inbox.Threads;
+            }
+            finally
+            {
+                _isLoading = false;
             }
         }
     }
